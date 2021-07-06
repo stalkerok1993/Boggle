cf016547cdcf350d94e8de7a17ee1c21a3962277
Serhii Bihun <s.bihun93@gmail.com>
2021-07-06 21:45:09 +0300

    Add text file with task definition, update test with cases described in the definition.

--- BoggleTests/BoggleTests.cs
+++ BoggleTests/BoggleTests.cs
@@ -6,6 +6,18 @@ namespace Tests
     public class BoggleTests
     {
         [TestMethod()]
+        public void CheckDefinitionTest()
+        {
+            var toCheck = new[] { "GEO", "BIAS", "ILNBIA", "BUNGIE", "BINS", "SINUS" };
+            var expecteds = new[] { true, true, true, false, false, false };
+
+            for (int i = 0; i < toCheck.Length; i++)
+            {
+                Assert.AreEqual(expecteds[i], new Boggle(CreateBoard1(), toCheck[i]).Check(), $"\nWrong check for the word \"{toCheck[i]}\"");
+            }
+        }
+
+        [TestMethod()]
         public void CheckTest()
         {
             var toCheck = new[] { "C", "EAR", "EARS", "BAILER", "ENLAREIAIBYORSCA", "CEREAL", "ROBES" };
@@ -13,11 +25,21 @@ namespace Tests
 
             for (int i = 0; i < toCheck.Length; i++)
             {
-                Assert.AreEqual(expecteds[i], new Boggle(CreateBoard(), toCheck[i]).Check(), $"\nWrong check for the word \"{toCheck[i]}\"");
+                Assert.AreEqual(expecteds[i], new Boggle(CreateBoard2(), toCheck[i]).Check(), $"\nWrong check for the word \"{toCheck[i]}\"");
             }
         }
+        private char[][] CreateBoard1()
+        {
+            return new char[][]
+            {
+                new[] { 'I', 'L', 'A', 'W' },
+                new[] { 'B', 'N', 'G', 'E' },
+                new[] { 'I', 'U', 'A', 'O' },
+                new[] { 'A', 'S', 'R', 'L' }
+            };
+        }
 
-        private char[][] CreateBoard()
+        private char[][] CreateBoard2()
         {
             return new char[][]
             {

4bbeb8168ba52030f9e94d66ff48849a1fc9c763
Serhii Bihun <s.bihun93@gmail.com>
2021-07-06 20:10:38 +0300

    Initial commit (solution template & unit test).

---
+++ Boggle/Boggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public class Boggle
+{
+    private char[][] Board;
+    private string Word;
+
+    public Boggle(char[][] board, string word)
+    {
+        Board = board;
+        Word = word;
+    }
+
+    public bool Check()
+    {
+        // code goes here
+        return false;
+    }
+}
---
+++ BoggleTests/BoggleTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BoggleTests
+    {
+        [TestMethod()]
+        public void CheckTest()
+        {
+            var toCheck = new[] { "C", "EAR", "EARS", "BAILER", "ENLAREIAIBYORSCA", "CEREAL", "ROBES" };
+            var expecteds = new[] { true, true, false, false, true, false, false };
+
+            for (int i = 0; i < toCheck.Length; i++)
+            {
+                Assert.AreEqual(expecteds[i], new Boggle(CreateBoard(), toCheck[i]).Check(), $"\nWrong check for the word \"{toCheck[i]}\"");
+            }
+        }
+
+        private char[][] CreateBoard()
+        {
+            return new char[][]
+            {
+                new[] { 'E', 'A', 'R', 'A' },
+                new[] { 'N', 'L', 'E', 'C' },
+                new[] { 'I', 'A', 'I', 'S' },
+                new[] { 'B', 'Y', 'O', 'R' }
+            };
+        }
+    }
+}
